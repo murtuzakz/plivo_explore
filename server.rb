e496e78c7094864f5e7c1c0395c3e3f8ea40a67d
Murtuza Kutub <murtuzafirst@gmail.com>
2013-11-14 17:47:25 +0530

    taking out building dial section into separate function

--- server.rb
+++ server.rb
@@ -2,5 +2,19 @@ require 'sinatra'
 
 post '/' do
    content_type 'text/xml'
-  "<Response><Dial><Number>#{params[:To]}</Number></Dial></Response>"
+   wrap_in_response do 
+    dial_builder(params[:To])
+   end
+end
+
+def dial_builder(phone_number)
+  "<Dial>\
+    <Number>\
+      #{params[:To]}\
+    </Number>\
+  </Dial>"
+end
+
+def wrap_in_response
+  "<Response>" + yield + "</Response>"
 end
