25573eae90b85eab8bc5c501c2b51c05cc8ee227
Murtuza Kutub <murtuzafirst@gmail.com>
2013-11-15 08:29:14 +0530

    added fucntionality of record as well

--- server.rb
+++ server.rb
@@ -3,8 +3,11 @@ require 'sinatra'
 post '/' do
    content_type 'text/xml'
    wrap_in_response do 
-    dial_builder(params[:To])
+    dial_builder(params[:To]) + recorder(params[:Record])
    end
+end
+
+post '/record' do
 end
 
 def dial_builder(phone_number)
@@ -13,6 +16,12 @@ def dial_builder(phone_number)
       #{params[:To]}\
     </Number>\
   </Dial>"
+end
+
+def recorder(to_record)
+  if to_record
+    "<Record action='#{request.env['HTTP_HOST']}/record'></Record>"
+  end
 end
 
 def wrap_in_response
